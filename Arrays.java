5bc3c3af5feea38e291d7e64459e121fb046a1d6
RaeDeanFullerton <101029021+RaeDeanFullerton@users.noreply.github.com>
2022-07-30 16:55:00 -0700

    array homework

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] myNumbers = {0,1,1,2,3,5,8,13,21};
+		int sum=0;
+		for (int i : myNumbers) {
+			sum +=i;	
+		}
+		System.out.println(sum);
+	}
+
+}
+ 
